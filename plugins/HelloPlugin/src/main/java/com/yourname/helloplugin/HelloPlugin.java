15860d0b6f767977d99f68ca51c5b3f3a9e754be
ParkJongJoon7128 <whdwnsdk8111@naver.com>
2026-03-28 23:18:01 +0900

    refactor: helloplugin-reload 커맨드 및 ReloadCommand 제거

--- plugins/HelloPlugin/src/main/java/com/yourname/helloplugin/HelloPlugin.java
+++ plugins/HelloPlugin/src/main/java/com/yourname/helloplugin/HelloPlugin.java
@@ -9,14 +9,10 @@ public class HelloPlugin extends JavaPlugin {
     public void onEnable() {
         getLogger().info("HelloPlugin enabled!");
 
-        // config.yml 기본값 저장
         saveDefaultConfig();
 
-        // 커맨드 등록
         getCommand("hello").setExecutor(new HelloCommand());
-        getCommand("helloplugin-reload").setExecutor(new ReloadCommand(this));
 
-        // 이벤트 리스너 등록
         getServer().getPluginManager().registerEvents(
                 new PlayerListener(this), this
         );
